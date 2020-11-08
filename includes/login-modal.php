a849cf64a35dd44e328c2b18a89652852df26783
Zekirija Alomerovic <zekirija17@gmail.com>
2020-11-08 04:35:33 +0100

    ad

--- includes/login-modal.php
+++ includes/login-modal.php
@@ -19,22 +19,19 @@
 <div class="modal-dialog modal-dialog-centered">
     <div class="login-modal modal-content">
     <a href="#" id="close-search" onclick="closeSearch()"><i class="fa fa-times" aria-hidden="true"></i></a>
-       <img class="img img-responsive" src="assets/images/logo.png" width="100">
        <form class="form-signin py-5" method="POST" >
-						  <div class="form-label-group">
-						  <label for="email">Korisnička Adresa: </label>
-							<input type="text" name="email" id="email" placeholder="e-mail adresa">
-						  </div>
-						  <div class="form-label-group py-3">
-							  <label for="password">Korisnička šifra: </label>
-							  <input type="password" name="password" id="password" placeholder="šifra korisnička">
-						  </div>
-						  <div class="custom-control custom-checkbox mb-3">
-							<input type="checkbox" class="custom-control-input" id="customCheck1">
-							<label class="custom-control-label" for="customCheck1">Zapamti me</label>
-						  </div>
-						  <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="prijava" >Prijavi Se</button>
-							<h4 class="font-weight-light py-3">Nemas racun ? <a class="register-one" href="register"> Registruj se</a></h4>
-						</form>
+			<div class="form-label-group">
+				<input class="w-100" type="text" name="email" id="email" placeholder="e-mail adresa">
+			</div>
+			<div class="form-label-group py-3">
+				<input class="w-100" type="password" name="password" id="password" placeholder="šifra korisnička">
+			</div>
+			<div class="custom-control custom-checkbox mb-3">
+				<input type="checkbox" class="custom-control-input" id="customCheck1">
+				<label class="custom-control-label text-white" for="customCheck1">Zapamti me</label>
+			</div>
+			<button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="prijava" >Prijavi Se</button>
+			<h4 class="font-weight-light py-3">Nemas racun ? <a class="register-one" href="register"> Registruj se</a></h4>
+		</form>
     </div>
 </div>
